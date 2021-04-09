8e49c7cf7efec324bee7de9a161cc02d279ad5e0
AtsushiKitano <aquamarinearia1010@gmail.com>
2021-04-09 20:47:55 +0900

    [mod] ディレクトリ構成の変更

--- Component/index.tsx
+++
@@ -1,7 +0,0 @@
-import {render} from "react-dom";
-import Chars from "./Component";
-import ClassChar from "./ClassComp";
-
-render(<Chars/>, document.getElementById("root"))
-render(<ClassChar/>, document.getElementById("root2"))
-
--- HelloWorld/index.tsx
+++
@@ -1,5 +0,0 @@
-import {render} from "react-dom";
-
-let element = <h1>Hello World</h1>;
-
-render(element, document.getElementById("root"))
